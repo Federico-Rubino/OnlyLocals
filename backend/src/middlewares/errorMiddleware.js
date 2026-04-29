c19d78aa5adb364bdabfa577f0d0333c13af1ad0
Federico Rubino <fdg.rubino@gmail.com>
2026-04-29 22:32:57 +0200

    add email formatting verification

--- backend/src/middlewares/errorMiddleware.js
+++ backend/src/middlewares/errorMiddleware.js
@@ -1,7 +1,14 @@
+const validator = require("validator");
+
+
 exports.validateRegisterData = (req, res, next) => {
   const data = req.body;
   if(!data.email || !data.name || !data.password || !data.surname || !data.bornDate ){
     return res.status(400).json({ message: "Missing fields" });
+  }
+
+  if (!validator.isEmail(data.email)) {
+  return res.status(400).json({ message: "Invalid email" });
   }
   next();
 };
