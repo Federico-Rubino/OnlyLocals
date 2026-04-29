d76d74c513a67dcb024c33656d2b16894db09803
Federico Rubino <fdg.rubino@gmail.com>
2026-04-29 09:56:28 +0200

    added jsDocs package and call

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const userRoutes = require('./routes/routesUser');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
+
 
 app.use(express.json());
 
 // routes
 app.use('/api/users', userRoutes);
+
+
+//swagger api ui
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0'
+        ,
+        info: {
+            title: 'Hello World'
+            ,
+            version: '1.0.0'
+            ,
+        },
+    },
+    apis: [__dirname + '/routes/*.js'], // files containing annotations as above
+};
+
+const swaggerDocument = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+console.log(swaggerOptions.apis);
 
 // error handler (sempre alla fine)
 app.use(errorMiddleware);
--- backend/src/routes/routesUser.js
+++ backend/src/routes/routesUser.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+/**
+* @openapi
+* /api/users/register::
+*   post:
+*       description: register a new user!
+*       responses:
+*           200:
+*               description: Returns resgistered user data.
+*/
 router.post('/register', userController.register);
+
 router.get('/', userController.getUsers);
 
 module.exports = router;
