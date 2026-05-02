7d9b2dad402862e3a50a5aa0d89d02605e406473
Federico Rubino <fdg.rubino@gmail.com>
2026-05-02 12:27:49 +0200

    Add authentication layer

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+exports.autenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    //token not provided
+    if (!token){
+        return res.status(401).json({ message: 'Access denied. No token provided.'});
+    }
+
+    //decode token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodeData) =>{
+        if (err) {
+            return res.status(403).json({message: 'Invalid or expired token. Login again'})
+        }
+
+        //return id decoded
+        req.user = decodedData;
+
+        next();
+    });
+};
