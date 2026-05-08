8636fff8951282c526aeeccacc6182f57b6401d0
Ilaria <basanisiilaria92@gmail.com>
2026-05-08 18:48:36 +0200

    addFeedback controller inside shop controller

--- backend/src/controllers/shopController.js
+++ backend/src/controllers/shopController.js
@@ -204,3 +204,38 @@ exports.deletePromotion = async (req, res) => {
     res.status(500).json({ message: err.message });
 }
 }
+
+exports.addFeedback = async (req,res)=>{
+    try{
+        const userId = req.user.userId;
+        const {shopId} = req.params;
+        const {voto, commento} = req.body;
+
+        if(!voto || voto<1 || voto>5){
+            return res.status(400).json({
+                success: false,
+                message: "Il voto deve essere compres tra 1 e 5"
+            });
+        }
+        const feedback = await shopService.addFeedback(userId, shopId, {voto, commento});
+
+        res.status(201).json({
+            success: true,
+            message: "Feedback aggiunto con successo",
+            data: feedback
+        });
+    }catch (err){
+        if(err.message === "Shop not found"){
+            return res.status(404).json({ success: false, message: err.message});
+        }
+        if(err.message === "User not found"){
+            return res.status(404).json({success: false, message: err.message});
+        }
+
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+};
